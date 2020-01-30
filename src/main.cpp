acecf9bdc65b06f8a001b13ada6a73a71ee60ae0
Murilo Baldi <murilobaldi@tutanota.com>
2020-01-30 07:31:25 -0300

    Add function to parse input file
    - Add function that reads and treats each input character from file
    - Add auxiliary functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <typeinfo>
+#include <vector>
+
+#define NCELLS 30000
+#define cell_t unsigned int
 
 // enum for command line arguments
 enum {
@@ -9,6 +12,96 @@ enum {
 	FILENAME,
 	NARGS
 };
+
+int signal_of(int n) {
+	if(n < 0)
+		return -1;
+	return 1;
+}
+
+void print_n(std::vector<cell_t> tape, int n) {
+	for(int i = 0; i < n; i++) {
+		std::cout << tape[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+void goto_matching_bracket(std::string::iterator &it) {
+	char ref, target;
+	int balance_counter = 0, inc;
+
+	if(*it == '[') {
+		ref = '[';
+		target = ']';
+		inc = 1;
+	}
+	else {
+		ref = ']';
+		target = '[';
+		inc = -1;
+	}
+
+	while(true) {
+		if(*it == ref)
+			balance_counter++;
+		else if(*it == target)
+			balance_counter--;
+
+		if(balance_counter == 0) {
+			// found the matching bracket
+			return;
+		}
+		// goes forward if looking for ']'
+		// goes backward if looking for '['
+		it += inc;
+	}
+	return;
+}
+
+void parse_file(std::string file_content) {
+	std::string::iterator it = file_content.begin();
+
+	std::vector<cell_t> tape(NCELLS, 0);
+	int pointer = 0, offset;
+
+	while(it != file_content.end()) {
+		switch(*it) {
+		case '>':
+			pointer++;
+			break;
+		case '<':
+			pointer--;
+			break;
+		case '+':
+			tape[pointer]++;
+			break;
+		case '-':
+			tape[pointer]--;
+			break;
+		case '.':
+			std::cout << (char)tape[pointer];
+			break;
+		case ',':
+			char c;
+			std::cin.get(c);
+			tape[pointer] = c;
+			break;
+		case '[':
+			if(tape[pointer] == 0) {
+				goto_matching_bracket(it);
+			}
+			break;
+		case ']':
+			if(tape[pointer] != 0) {
+				goto_matching_bracket(it);
+			}
+			break;
+		default:
+			break;
+		}
+		it++;
+	}
+}
 
 std::string read_file(std::string filename) {
 	// opening given file
@@ -38,4 +131,6 @@ int main(int argc, char *argv[]) {
 	}
 
 	std::string file = read_file(argv[FILENAME]);
+
+	parse_file(file);
 }
